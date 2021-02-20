c22f2abc9b22312c41582a5081a2a2acb26d9635
MHDSAEEDRABID <saeedrubid@gmail.com>
2021-02-20 13:57:42 +0200

    Complete FoodType Upsert and View Page for Admin Dashboard

--- MainWeb/MainWeb/Controller/CategoryController.cs
+++ MainWeb/MainWeb/Controller/CategoryController.cs
@@ -1,10 +1,5 @@
 ﻿using DataAccess.Data.Repository.IRepository;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MainWeb.Controller
 {
---
+++ MainWeb/MainWeb/Controller/FoodTypeController.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using DataAccess.Data.Repository.IRepository;
+
+namespace MainWeb.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodTypeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FoodTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.FoodType.GetAll() });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.FoodType.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+            _unitOfWork.FoodType.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete success." });
+        }
+    }
+}
